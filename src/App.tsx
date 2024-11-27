70bf917ed241bb794224ecab41b715ee8ad962fd
Egeeoz <ozakdemirege@hotmail.com>
2024-11-27 20:45:53 +0100

    Add footer

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,7 @@ import Navbar from './components/Navbar';
 import Login from './pages/Login';
 import Signup from './pages/Signup';
 import 'react-toastify/dist/ReactToastify.css';
+import Footer from './components/Footer';
 
 function App() {
   return (
@@ -16,6 +17,7 @@ function App() {
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
       </Routes>
+      <Footer />
       <ToastContainer
         transition={Slide}
         style={{
